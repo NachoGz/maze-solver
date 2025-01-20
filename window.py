a911b6feb007cf2c7fd373779bba3d2400e93cb8
NachoGz <ignaciogomezf1@gmail.com>
2025-01-20 19:42:54 -0300

    made fill_color black by default

--- window.py
+++ window.py
@@ -25,5 +25,5 @@ class Window():
     def close(self):
         self.running = False
 
-    def draw_line(self, line: Line, fill_color: str):
+    def draw_line(self, line: Line, fill_color: str = "black"):
         line.draw(self.canvas, fill_color)
