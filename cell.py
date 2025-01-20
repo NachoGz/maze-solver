a35d343dbf06ab6ffae055bef0813971e7fbc59c
NachoGz <ignaciogomezf1@gmail.com>
2025-01-20 13:02:22 -0300

    changed win to option with None as default to be able to do testing and added getters for coordinates

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from point import Point
 from line import Line
 class Cell():
-    def __init__(self, x1, x2, y1, y2, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+    def __init__(self, x1, x2, y1, y2, win=None, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
         
         self.has_left_wall = has_left_wall
         self.has_right_wall = has_right_wall
@@ -12,25 +12,27 @@ class Cell():
         self.__x2 = x2
         self.__y2 = y2
         self.__win = win
+        self.visited = False
 
     def draw(self):
-        top_left_corner = Point(self.__x1, self.__y1)
-        bottom_left_corner = Point(self.__x1, self.__y2)
-        top_right_corner = Point(self.__x2, self.__y1)
-        bottom_right_corner = Point(self.__x2, self.__y2)
+        if self.__win:
+            top_left_corner = Point(self.__x1, self.__y1)
+            bottom_left_corner = Point(self.__x1, self.__y2)
+            top_right_corner = Point(self.__x2, self.__y1)
+            bottom_right_corner = Point(self.__x2, self.__y2)
 
-        if self.has_left_wall:
-            wall = Line(top_left_corner, bottom_left_corner)
-            self.__win.draw_line(wall, "black")
-        if self.has_right_wall:
-            wall = Line(top_right_corner, bottom_right_corner)
-            self.__win.draw_line(wall, "black")
-        if self.has_top_wall:
-            wall = Line(top_left_corner, top_right_corner)
-            self.__win.draw_line(wall, "black")
-        if self.has_bottom_wall:
-            wall = Line(bottom_left_corner, bottom_right_corner)
-            self.__win.draw_line(wall, "black")
+            if self.has_left_wall:
+                wall = Line(top_left_corner, bottom_left_corner)
+                self.__win.draw_line(wall, "black")
+            if self.has_right_wall:
+                wall = Line(top_right_corner, bottom_right_corner)
+                self.__win.draw_line(wall, "black")
+            if self.has_top_wall:
+                wall = Line(top_left_corner, top_right_corner)
+                self.__win.draw_line(wall, "black")
+            if self.has_bottom_wall:
+                wall = Line(bottom_left_corner, bottom_right_corner)
+                self.__win.draw_line(wall, "black")
 
     def get_center(self):
         cx = (self.__x1 + self.__x2) / 2
@@ -45,4 +47,15 @@ class Cell():
             self.__win.draw_line(path, "gray")
         else:
             self.__win.draw_line(path, "red")
-            
+    
+    def get_top_left_x(self):
+        return self.__x1
+    
+    def get_bottom_right_x(self):
+        return self.__x2
+    
+    def get_top_left_y(self):
+        return self.__y1
+    
+    def get_bottom_right_y(self):
+        return self.__y2
